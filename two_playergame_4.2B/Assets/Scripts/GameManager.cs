9fc04175d212c568afe22f41156f0183202becf0
DavidFuzzy <32672953+DavidFuzzy@users.noreply.github.com>
2017-12-09 11:56:22 +0100

    commit 3

--- two_playergame_4.2B/Assets/Scripts/GameManager.cs
+++ two_playergame_4.2B/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 	public static int PlayerScore1 = 0;
@@ -15,7 +16,7 @@ public class GameManager : MonoBehaviour {
 	}
 
 	public static void Score (string wallID) {
-		if (wallID == "RightWall")
+		if (wallID == "right_score")
 		{
 			PlayerScore1++;
 		} else
@@ -29,20 +30,17 @@ public class GameManager : MonoBehaviour {
 		GUI.Label(new Rect(Screen.width / 2 - 150 - 12, 20, 100, 100), "" + PlayerScore1);
 		GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 20, 100, 100), "" + PlayerScore2);
 
-		if (GUI.Button(new Rect(Screen.width / 2 - 60, 35, 120, 53), "RESTART"))
-		{
-			PlayerScore1 = 0;
-			PlayerScore2 = 0;
-			theBall.SendMessage("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);
-		}
 
-		if (PlayerScore1 == 10)
+
+		if (PlayerScore1 == 3)
 		{
-			GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER ONE WINS");
+			GUI.Label(new Rect(Screen.width / 2 - -30, 200, 2000, 1000), "PLAYER TWO WINS");
 			theBall.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
-		} else if (PlayerScore2 == 10)
+			SceneManager.LoadScene("OtherSceneName", LoadSceneMode.Additive);
+
+		} else if (PlayerScore2 == 3)
 		{
-			GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER TWO WINS");
+			GUI.Label(new Rect(Screen.width / 2 - -30, 200, 2000, 1000), "PLAYER ONE WINS");
 			theBall.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
 		}
 	}
